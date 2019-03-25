926a987a467827f79bd404f3251667c4deb8da57
Mark Vayngrib <mark@tradle.io>
2019-03-25 19:50:11 -0400

    fix: go/serve.sh

--- go/serve.sh
+++ go/serve.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-export DYLD_LIBRARY_PATH=../lib
-export CGO_CFLAGS=-I../include
-export CGO_LDFLAGS=-L../lib
+
+HERE=$(dirname $0)
+
+export DYLD_LIBRARY_PATH=$(realpath "$HERE/../lib")
+export LD_LIBRARY_PATH=$(realpath "$HERE/../lib")
+export CGO_CFLAGS=-I$(realpath "$HERE/../include")
+export CGO_LDFLAGS=-L$(realpath "$HERE/../lib")
 
 PORT=${1-10001}
 
-go run -v roc_server.go serve $PORT
+go run -v $(dirname $0)/roc_server.go serve $PORT
